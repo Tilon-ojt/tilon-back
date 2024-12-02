e1b82f1597614b4591a49d054c229466e0760404
Yuming <102581107+Yuminyumin@users.noreply.github.com>
2024-12-02 15:56:34 +0900

    feat: 정적 리소스 경로 설정

--- src/main/java/com/tilon/ojt_back/config/WebConfig.java
+++ src/main/java/com/tilon/ojt_back/config/WebConfig.java
@@ -13,5 +13,12 @@ public class WebConfig implements WebMvcConfigurer {
         .allowedOrigins("http://localhost:3000") //내 포트가 아니라 들어오는 url 로 지정해줘야 한다. 예를 들어 요청을 하는 프론트의 url로 지정 
         .allowedMethods("*");
     }
+
+    // 정적 리소스 경로 설정
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**") // "/static/**" 경로로 들어오는 요청을 처리
+                .addResourceLocations("classpath:/static/"); // Spring Boot의 기본 정적 리소스 경로 사용
+    }
 }
 
--- src/main/java/com/tilon/ojt_back/controller/homepage/PostController.java
+++ src/main/java/com/tilon/ojt_back/controller/homepage/PostController.java
@@ -3,6 +3,7 @@ package com.tilon.ojt_back.controller.homepage;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +20,8 @@ public class PostController {
     @Autowired
     private PostService postService;
 
-    // post 조회
-    @GetMapping("/post")
+    // posts 조회
+    @GetMapping("/posts")
     public List<PostResponseDTO> getPost(
         @RequestParam(name = "category") PostCategory category){
         return postService.getPost(category);
