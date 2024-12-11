6231d48ea00e6b33cc4ff70e103e24d5ecc805c3
Yuming <102581107+Yuminyumin@users.noreply.github.com>
2024-12-11 13:50:00 +0900

    feat: 전역 예외처리

---
+++ src/main/java/com/tilon/ojt_back/exception/CustomException.java
@@ -0,0 +1,20 @@
+package com.tilon.ojt_back.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return errorCode.getHttpStatus();
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
--- src/main/java/com/tilon/ojt_back/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tilon/ojt_back/exception/GlobalExceptionHandler.java
@@ -3,14 +3,14 @@ package com.tilon.ojt_back.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<ErrorResponseDTO> handleResponseStatusException(ResponseStatusException e) {
-        ErrorCode errorCode = ErrorCode.valueOf(e.getStatusCode().toString());
-        return ResponseEntity.status(e.getStatusCode()).body(new ErrorResponseDTO(errorCode));
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponseDTO> handleCustomException(CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(new ErrorResponseDTO(errorCode));
     }
 }
+
